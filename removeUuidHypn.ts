bd5079408e213368bd3abaad43159689bf025f8f
tkk <tokiko.develop@gmail.com>
2021-05-19 19:13:25 +0900

    First commit

---
+++ removeUuidHypn.ts
@@ -0,0 +1,13 @@
+import { v4 } from "https://deno.land/std@0.95.0/uuid/mod.ts";
+
+const inputUuid = Deno.args[0];
+
+// Validate a v4 uuid.
+const isValid = v4.validate(inputUuid);
+if (isValid) {
+    console.log(inputUuid.replace(/-/g, ''));
+    Deno.exit(0);
+} else {
+    console.log(inputUuid, isValid);
+    Deno.exit(1);
+}
